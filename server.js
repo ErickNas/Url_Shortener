e9ac7c079642d548a158e9a507fc5704812e9e44
Erick Bezerra <erick.bezerra@conveste.com.br>
2022-08-17 10:21:05 -0300

    Adicionando swagger

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const server = express();
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 const path = require('path');
+
+const server = express();
 const port = process.env.PORT || 3000;
 
 const indexRouter = require('./src/routes/index');
+const spec = YAML.load('./swagger.yml');
 
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'ejs');
 
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(spec));
 server.use('/', indexRouter);
 
 server.listen(port);
